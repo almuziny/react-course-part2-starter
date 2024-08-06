d62ee6abf67ddbadf3c0beaad1c9c63eae11a17c
almuziny <ma.almuzainy@gmail.com>
2024-08-06 09:35:33 +0300

    Optimistic Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
+import TodoList from "./react-query/TodoList";
 
 function App() {
   return (
     <>
       <TodoForm />
-      <PostList />
+      <TodoList />
     </>
   );
 }
