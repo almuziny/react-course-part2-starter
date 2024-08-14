ec246f42fa95856a64f1348bf7d9345f742d0554
almuziny <ma.almuzainy@gmail.com>
2024-08-14 09:31:36 +0300

    Exercise - Working with Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import TasksContext from "./state-management/contexts/tasksContext";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
 import tasksReducer from "./state-management/reducers/tasksReducer";
+import authReducer from "./state-management/reducers/authReducer";
+import AuthContext from "./state-management/contexts/authContext";
 
 function App() {
-  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  const [tasks, taskDispatch] = useReducer(tasksReducer, []);
+  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <TasksContext.Provider value={{ tasks, dispatch }}>
-      <NavBar />
-      <HomePage />
+    <TasksContext.Provider value={{ tasks, dispatch: taskDispatch }}>
+      <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+        <NavBar />
+        <HomePage />
+      </AuthContext.Provider>
     </TasksContext.Provider>
   );
 }
